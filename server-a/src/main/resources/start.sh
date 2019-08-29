e72eee827a415322b036613725baf27d5832d619
yinbucheng <yin.chong@intellif.com>
2019-08-29 21:02:12 +0800

    修改Dockerfile

--- server-a/src/main/resources/start.sh
+++ server-a/src/main/resources/start.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 java -XX:+UseConcMarkSweepGC -Xmx1024m -Xms1024m  -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:./logs/java_gc.log -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Denv=dev   -Duser.timezone=Asia/Shanghai  -jar server-a.jar --spring.profiles.active=default
 
