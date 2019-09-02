a657e5a4b1c8ff27b2507ae806dd47e87bbcec64
yinbucheng <yin.chong@intellif.com>
2019-09-02 12:37:11 +0800

    移植到k8s环境中

---
+++ zuul/src/main/java/cn/bucheng/zuul/filter/TokenFilter.java
@@ -0,0 +1,57 @@
+package cn.bucheng.zuul.filter;
+
+import brave.Tracer;
+import brave.Tracing;
+import brave.propagation.TraceContext;
+import com.netflix.zuul.ZuulFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TokenFilter extends ZuulFilter {
+
+    /**
+     * 过滤器的类型，它决定过滤器在请求的哪个生命周期中执行。
+     * 这里定义为pre，代表会在请求被路由之前执行。
+     *
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * filter执行顺序，通过数字指定。
+     * 数字越大，优先级越低。
+     *
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 判断该过滤器是否需要被执行。这里我们直接返回了true，因此该过滤器对所有请求都会生效。
+     * 实际运用中我们可以利用该函数来指定过滤器的有效范围。
+     *
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤器的具体逻辑
+     *
+     * @return
+     */
+    @Override
+    public Object run() {
+        log.info("zuul filter run,traceId");
+        return null;
+    }
+}
